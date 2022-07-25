df564d9e0b0a6c5d7ef1e782056685b902d1f8cc
futao <1185172056@qq.com>
2022-07-25 22:52:14 +0800

    init

---
+++ src/views/A.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>defineComponent、setup、ref</h1>
+  cur count is: {{ count }}
+  <button @click="addOne">add one</button>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+  setup() {
+    // 通过ref函数定义一个基本类型的响应式对象
+    const count = ref<number>(0)
+    // 定义一个方法,ref定义的响应式数据需要通过ref访问
+    const addOne = function () {
+      count.value++
+    }
+    //setup方法返回一个对象，可以包含属性和方法
+    return {
+      addOne,
+      count
+    }
+  }
+})
+</script>
---
+++ src/views/B.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>reactive</h1>
+  {{ user }}
+  <button @click="addWife">再来一房</button>
+
+  <ul>
+    <li v-for="school in schools">
+      {{ school }}
+    </li>
+  </ul>
+</template>
+
+
+<script lang="ts">
+import {defineComponent, reactive} from "vue";
+
+export default defineComponent({
+  setup() {
+    // 返回的是一个响应式代理对象
+    // 基于ES6 Proxy实现
+    const user = reactive({
+      username: '李达康',
+      age: 18,
+      wife: [{
+        username: '小甜甜',
+        age: 18,
+        hobby: [
+          '抽烟',
+          '喝酒',
+          '钢丝球'
+        ]
+      }]
+    })
+
+    const schoolArr = [{
+      name: 'ecjtu',
+      address: '江西省南昌市',
+      xz: user
+    }]
+
+    const schools = reactive(schoolArr)
+
+    const addWife = () => {
+      // reactive 定义的响应式数据不需要通过.value访问
+      user.wife.push(user.wife[0])
+    }
+    return {user, addWife, schools}
+  }
+})
+
+</script>
